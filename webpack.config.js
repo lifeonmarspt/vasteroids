b2fa0547a43dab65f56a3fa0d38f0f258416bbbc
Hugo Peixoto <hugo.peixoto@gmail.com>
2016-08-10 17:53:18 +0100

    Adds webpack, babel and es2015

--- webpack.config.js
+++ webpack.config.js
@@ -2,12 +2,19 @@ var path = require('path');
 
 module.exports = {
   entry: './js/entry.js',
+  devtool: 'source-map',
   output: {
     path: __dirname,
     filename: 'bundle.js'
   },
 
   module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'source-map-loader'
+      }
+    ],
     loaders: [
       {
         test: /\.js$/,
@@ -16,5 +23,4 @@ module.exports = {
       }
     ]
   }
-
 }
