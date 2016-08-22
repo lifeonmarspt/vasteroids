9afb9b3647ee8340f886499b714082479eeb3309
Hugo Peixoto <hugo.peixoto@gmail.com>
2016-08-22 16:56:56 +0100

    Positions the asteroids in a strip around the player

--- js/throwingAsteroidsAtYouTest.js
+++ js/throwingAsteroidsAtYouTest.js
@@ -1,8 +1,28 @@
+function normal_random(n) {
+  var x = 0;
+  for (var i = 0; i < n; i++) {
+    x += Math.random();
+  }
+
+  return x / n;
+}
+
+function polar2cartesian(longitude, latitude, radius) {
+  return {
+    x: radius * Math.sin(longitude) * Math.cos(latitude),
+    z: -radius * Math.cos(longitude) * Math.cos(latitude),
+    y: radius * Math.sin(latitude)
+   }
+}
+
 setInterval(() => {
+  var longitude = normal_random(2) * Math.PI * 2;
+  var latitude = normal_random(10) * Math.PI/2 - Math.PI/4;
+
   var ast = document.createElement('a-entity');
   ast.setAttribute('asteroid', true);
   ast.setAttribute('distance-limiter', "maxDistance: 100");
-  ast.setAttribute("position", '0 0 -50');
+  ast.setAttribute("position", polar2cartesian(longitude, latitude, 50.0));
 
   var animRot = document.createElement('a-animation');
   animRot.setAttribute('attribute', 'rotation');
