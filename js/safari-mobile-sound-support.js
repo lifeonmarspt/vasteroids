f7d0445f5b4d05ce42b417664f50ade18a294791
João Anes <joao.anes@gmail.com>
2016-09-07 17:03:03 +0100

    Refactored iPhone sound support

--- js/safari-mobile-sound-support.js
+++ js/safari-mobile-sound-support.js
@@ -1,9 +1,20 @@
-if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
-  window.addEventListener('touchend', function() {
+//Due to a battery safety issue, all sound  in a webpage starts muted unless
+//a sound is played inside a non-synthetic touch event
+
+function createAndPlayNoise() {
+    if (THREE.AudioContext.state !== "suspended")
+    {
+        window.removeEventListener('touchend', createAndPlayNoise)
+        return
+    }
+
     var buffer = THREE.AudioContext.createBuffer(1, 1, 22050);
     var source = THREE.AudioContext.createBufferSource();
     source.buffer = buffer;
     source.connect(THREE.AudioContext.destination);
     source.play();
-  }, false);
+}
+
+if (/iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream) {
+  window.addEventListener('touchend', createAndPlayNoise, false);
 }
